973adf3c5dbc1a2a9984b4123ee57f3178c5a1e4
Temitope Omotunde <topeomot@gmail.com>
2021-10-23 05:42:38 +0100

    refactor Athena to class

--- src/services/Athena.ts
+++ src/services/Athena.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
-import { IAthenaResponse } from 'src/models/interfaces';
+import IHealthDataService, { IDimension, IResponse } from 'src/models/IHealthDataInterface';
 import { URL } from 'url';
 
 const baseUrl = process.env.ATHENA_API || 'http://apps.who.int/gho/athena/api/';
 
-export const Athena = {
-  getData: function (path: string) {
+export default class Athena implements IHealthDataService{
+  baseUrl: string = process.env.ATHENA_API || 'http://apps.who.int/gho/athena/api/';
+
+  constructor(){}
+  
+  getData(path: string): Promise<IDimension[]> {
     const url = new URL(path, baseUrl);
 
     return new Promise((resolve, reject) => {
@@ -14,7 +18,7 @@ export const Athena = {
         url: url.searchParams.keys.length === 0 ? `${url.href}?format=json` : `${url.href}&format=json`,
       })
         .then((res) => {
-          resolve((res.data as IAthenaResponse).dimension);
+          resolve((res.data as IResponse).dimension);
         })
         .catch((err) => {
           reject(err);
